13585a156e48f676c424513474e9a57c44bc5010
ailishev <114398805+ailishev@users.noreply.github.com>
2026-04-30 17:18:09 +0300

    Switch auth UI to DB-backed login/register page and bypass Telegram auth flow

--- src/index.ts
+++ src/index.ts
@@ -618,26 +618,7 @@ function setDocumentLangPackProperties(langPack: LangPackDifference.langPackDiff
 
     let pagePromise: Promise<void>;
     // langPromise.then(async() => {
-    switch(authState._) {
-      case 'authStateSignIn':
-        pagePromise = (await import('./pages/pageSignIn')).default.mount();
-        break;
-      case 'authStateSignQr':
-        pagePromise = (await import('./pages/pageSignQR')).default.mount();
-        break;
-      case 'authStateAuthCode':
-        pagePromise = (await import('./pages/pageAuthCode')).default.mount(authState.sentCode);
-        break;
-      case 'authStatePassword':
-        pagePromise = (await import('./pages/pagePassword')).default.mount();
-        break;
-      case 'authStateSignUp':
-        pagePromise = (await import('./pages/pageSignUp')).default.mount(authState.authCode);
-        break;
-      case 'authStateSignImport':
-        pagePromise = (await import('./pages/pageSignImport')).default.mount(authState.data);
-        break;
-    }
+    pagePromise = (await import('./pages/pageDbAuth')).default.mount();
     // });
 
     if(scrollable) {
